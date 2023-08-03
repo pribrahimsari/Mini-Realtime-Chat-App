61ec59e311c32ae3df6f440b8b3bde6cc0beebe3
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-04 02:21:29 +0300

    prettier line indents for Messages ts in BE

--- backend/lib/Messages.ts
+++ backend/lib/Messages.ts
@@ -3,44 +3,44 @@ import _ from "lodash";
 import getRedisClient from "../clients/redis";
 
 class Messages {
-    private client: any;
+  private client: any;
 
-    constructor() {
-        this.client = getRedisClient();
-    }
+  constructor() {
+    this.client = getRedisClient();
+  }
 
-    upsert({ message }: { message: string }): void {
-        this.client.hset(
-            "messages",
-            shortid.generate(),
-            JSON.stringify({
-                message,
-                when: Date.now(),
-            }),
-            (err: Error | null) => {
-                if (err) {
-                    console.error(err);
-                }
-            }
-        );
-    }
+  upsert({ message }: { message: string }): void {
+    this.client.hset(
+      "messages",
+      shortid.generate(),
+      JSON.stringify({
+        message,
+        when: Date.now(),
+      }),
+      (err: Error | null) => {
+        if (err) {
+          console.error(err);
+        }
+      },
+    );
+  }
 
-    list(callback: (messageList: any[]) => void): void {
-        let messageList: any[] = [];
+  list(callback: (messageList: any[]) => void): void {
+    let messageList: any[] = [];
 
-        this.client.hgetall("messages", (err: Error | null, messages: any) => {
-            if (err) {
-                console.error(err);
-                return callback([]);
-            }
+    this.client.hgetall("messages", (err: Error | null, messages: any) => {
+      if (err) {
+        console.error(err);
+        return callback([]);
+      }
 
-            for (let message in messages) {
-                messageList.push(JSON.parse(messages[message]));
-            }
+      for (let message in messages) {
+        messageList.push(JSON.parse(messages[message]));
+      }
 
-            return callback(_.orderBy(messageList, "when", "asc"));
-        });
-    }
+      return callback(_.orderBy(messageList, "when", "asc"));
+    });
+  }
 }
 
 export default new Messages();
