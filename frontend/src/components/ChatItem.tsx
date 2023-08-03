f3ade1640cf2f14f75fd6af97a6d53d53972192a
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-03 23:34:28 +0300

    map ChatList through ChatItem components

--- frontend/src/components/ChatItem.tsx
+++ frontend/src/components/ChatItem.tsx
@@ -1,7 +1,8 @@
 import styles from "./style.module.css";
+import { Message } from "../types/message.types";
 
-const ChatItem = () => {
-  return <div className={styles.chatItem}></div>;
+const ChatItem = ({ message }: { message: Message }) => {
+  return <div className={`${styles.chatItem}`}>{message.text}</div>;
 };
 
 export default ChatItem;
