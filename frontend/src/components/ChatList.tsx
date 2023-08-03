352225ca31e4f64596b3f232f8f27531010f6f95
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-03 21:00:40 +0300

    create empty components

---
+++ frontend/src/components/ChatList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatList = () => {
+    return (
+        <div>
+            TODO: Chat List
+        </div>
+    );
+};
+
+export default ChatList;
---
+++ frontend/src/context/ChatContext.tsx
@@ -0,0 +1,25 @@
+import React, {createContext, useContext, useState} from "react";
+
+type TChatContext = {
+  messages: null;
+  setMessages: React.Dispatch<React.SetStateAction<null>>;
+}
+
+const ChatContext = createContext<TChatContext | null>(null);
+
+export const ChatProvider = ({children}:{children: React.ReactElement}) => {
+  const [messages, setMessages] = useState(null);
+
+  const values = {
+    messages,
+    setMessages
+  }
+
+  return (
+      <ChatContext.Provider value={values}>
+        {children}
+      </ChatContext.Provider>
+  );
+}
+
+export const useChatContext = () => useContext(ChatContext);
