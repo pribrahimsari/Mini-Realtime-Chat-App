0fcd41be4126087c5dc01e5e5fd4242b004991e7
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-03 23:55:09 +0300

    create a basic form to handle message to be sent

--- frontend/src/components/ChatForm.tsx
+++ frontend/src/components/ChatForm.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./style.module.css";
 
 const ChatForm = () => {
-  return <div>TODO: Chat Form</div>;
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // todo
+    // console.debug({ message });
+    setMessage("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className={styles.textInput}
+          type="text"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default ChatForm;
