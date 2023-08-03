fda914227c03a5f04f9c935db92af5d55a04e64c
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-04 00:46:31 +0300

    render the msg user sent on right side of the msg list component

--- frontend/src/components/ChatForm.tsx
+++ frontend/src/components/ChatForm.tsx
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 import styles from "./style.module.css";
+import { sendMessage } from "../api/socketApi";
+import { useChatContext } from "../context/ChatContext";
 
 const ChatForm = () => {
   const [message, setMessage] = useState("");
 
+  const { setMessages } = useChatContext();
+
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    // todo
-    // console.debug({ message });
-    setMessage("");
+    if (message) {
+      setMessages((prevState) => [
+        ...prevState,
+        {
+          id: (prevState.at(-1)?.id || 0) + 1,
+          text: message,
+          fromMe: true,
+        },
+      ]);
+      sendMessage(message);
+      setMessage("");
+    }
   };
 
   return (
