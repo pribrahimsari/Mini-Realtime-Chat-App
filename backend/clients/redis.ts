d6a185e827a15098f8c7e645b0f8c5e67a180168
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-04 02:20:48 +0300

    install correct version of redis

--- backend/clients/redis.ts
+++ backend/clients/redis.ts
@@ -1,18 +1,5 @@
-import {createClient} from "redis";
+const redis = require("redis");
 
-const getRedisClient = () => {
-	// const host = process.env.REDIS_HOST;
-	// const port = process.env.REDIS_PORT;
-	// const password = process.env.REDIS_PASS;
-
-	return createClient(
-	// 	{
-	// 	host: process.env.REDIS_HOST,
-	// 	port: process.env.REDIS_PORT,
-	// 	password: process.env.REDIS_PASS,
-	// 	db: 1,
-	// }
-	);
-};
+const getRedisClient = () => redis.createClient();
 
 export default getRedisClient;
