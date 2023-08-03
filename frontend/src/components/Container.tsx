bef44576766b451abc7143399fcb122dd1d8a286
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-03 21:00:40 +0300

    create empty components

---
+++ frontend/src/components/Container.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ChatList from "./ChatList";
+import ChatForm from "./ChatForm";
+
+const Container = () => {
+    return (
+        <div>
+            <ChatList />
+            <ChatForm />
+        </div>
+    );
+};
+
+export default Container;
