79274acc5a7f547ea048fe7d73a9becaffedae4d
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-04 01:13:14 +0300

    prettier code 2

--- frontend/src/components/ChatList.tsx
+++ frontend/src/components/ChatList.tsx
@@ -5,8 +5,6 @@ import ChatItem from "./ChatItem";
 const ChatList = () => {
   const { messages } = useChatContext();
 
-  console.debug({ messages });
-
   return (
     <div className={styles.chatlist}>
       {messages.map((message) => (
