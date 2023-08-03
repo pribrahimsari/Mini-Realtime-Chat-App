b7661d291df97572e3107f46ab4f8ac25406b512
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-04 00:46:07 +0300

    init socket connection on container & subscribe to receive msg channel

--- frontend/src/components/Container.tsx
+++ frontend/src/components/Container.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ChatList from "./ChatList";
 import ChatForm from "./ChatForm";
 import styles from "./style.module.css";
+import { init, subscribeChat } from "../api/socketApi";
+import { useChatContext } from "../context/ChatContext";
 
 const Container = () => {
+  const { setMessages } = useChatContext();
+
+  useEffect(() => {
+    init();
+    subscribeChat((message) => {
+      setMessages((prevState) => [
+        ...prevState,
+        { id: (prevState.at(-1)?.id || 0) + 1, text: message },
+      ]);
+    });
+  }, [setMessages]);
+
+  // todo: double render
+  console.debug("rendered");
+
   return (
     <div className={styles.container}>
       <ChatList />
