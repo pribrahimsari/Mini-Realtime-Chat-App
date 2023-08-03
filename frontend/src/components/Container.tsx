ca1c1f18dda17a3f9ba1b790cab1ed5179486453
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-03 23:54:46 +0300

    better container style to center chat app in page

--- frontend/src/components/Container.tsx
+++ frontend/src/components/Container.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ChatList from "./ChatList";
 import ChatForm from "./ChatForm";
+import styles from "./style.module.css";
 
 const Container = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <ChatList />
       <ChatForm />
     </div>
