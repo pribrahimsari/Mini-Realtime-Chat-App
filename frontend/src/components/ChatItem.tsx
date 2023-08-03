1cbac6a09537e70f5f4f85e4f6f8902d2f20a544
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-04 00:44:41 +0300

    add fromMe boolean type to a msg txt

--- frontend/src/components/ChatItem.tsx
+++ frontend/src/components/ChatItem.tsx
@@ -2,7 +2,14 @@ import styles from "./style.module.css";
 import { Message } from "../types/message.types";
 
 const ChatItem = ({ message }: { message: Message }) => {
-  return <div className={`${styles.chatItem}`}>{message.text}</div>;
+  return (
+    <div
+      className={`${styles.chatItem} ${message.fromMe ? styles.right : ""}
+  `}
+    >
+      {message.text}
+    </div>
+  );
 };
 
 export default ChatItem;
